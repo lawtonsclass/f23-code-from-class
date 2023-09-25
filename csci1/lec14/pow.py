4da533ee8db80e7be5858b323d66cdcdad331204
Lawton Nichols <lawtonnichols@gmail.com>
2023-09-25 16:20:49 -0700

    .

---
+++ csci1/lec14/pow.py
@@ -0,0 +1,8 @@
+# get the first # from the user (as an int)
+num1 = int(input("Enter the first num: "))
+
+# get the first # from the user (as an int)
+num2 = int(input('Enter the second num: '))
+
+# calculate num1 ^ num2 and print the result
+print('The result is', num1 ** num2)
---
+++ csci1/lec14/square.py
@@ -0,0 +1,27 @@
+import turtle
+
+print(turtle.position())
+
+# make the turtle actually look like a turtle
+turtle.shape('turtle')
+
+# make the turtle go fast
+turtle.speed('fast')
+
+# Mr. Turtle, please move in your current direction by 100 units
+turtle.forward(100)
+
+print(turtle.position())
+
+# turn left 90 degree
+turtle.left(90)
+
+turtle.forward(100)
+
+turtle.left(90)
+
+turtle.forward(100)
+
+turtle.left(90)
+
+turtle.forward(100)
